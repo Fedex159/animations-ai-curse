0d64bfca6865b4f0ccde45d61f1e521e612a6a3d
Federico Avelin <fede.avelin.dev@gmail.com>
2025-05-11 20:06:03 -0300

    refactor: use lenis provider and add snap-y styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import ReactLenis from "lenis/react";
+import { LenisProvider } from "@/shared/providers/LenisProvider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -24,11 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="snap-y snap-mandatory">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ReactLenis root>{children}</ReactLenis>
+        <LenisProvider>{children}</LenisProvider>
       </body>
     </html>
   );
---
+++ shared/providers/LenisProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import ReactLenis, { LenisRef } from "lenis/react";
+import { frame } from "motion";
+import { PropsWithChildren, useEffect, useRef } from "react";
+
+export const LenisProvider = ({ children }: PropsWithChildren) => {
+  const lenisRef = useRef<LenisRef>(null);
+
+  useEffect(() => {
+    function update(data: { timestamp: number }) {
+      const time = data.timestamp;
+      lenisRef.current?.lenis?.raf(time);
+    }
+
+    frame.update(update, true);
+
+    return;
+  }, []);
+
+  return (
+    <ReactLenis root options={{ autoRaf: false, smoothWheel: false }}>
+      {children}
+    </ReactLenis>
+  );
+};
